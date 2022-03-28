0bb39cdbf78db9793e56f371166661b696bebf9a
grabbytabby@zohomail <100648556+grabbytabby@users.noreply.github.com>
2022-03-28 15:05:19 -0700

    Create node.js

---
+++ https:/en.wikipedia.org/wiki/Template:Bibcode/node.js/node.js
@@ -0,0 +1,19 @@
+              The code has a fixed length of 19 characters and has the form YYYYJJJJJVVVVMPPPPA
+             where YYYY is the four-digit year of the reference and JJJJJ is a code indicating where the reference was published.
+        In the case of a journal reference, VVVV is the volume number, M indicates the section of the journal where the reference was published 
+     (e.g., L for a letters section), PPPP gives the starting page number, and A is the first letter of the last name of the first author. Periods
+            (.) are used to fill unused fields and to pad fields out to their fixed length if too short; 
+      padding is done on the right for the publication code and on the left for the volume number and page number.
+    [1][2] Page numbers greater than 9999 are continued in the M column. The 6-digit article ID numbers 
+                (in lieu of page numbers) used by the Physical Review publications since the late 1990s are treated as follows: 
+        The first two digits of the article ID, corresponding to the issue number, 
+            are converted to a lower-case letter (01 = a, etc.) and inserted into column M. 
+      The remaining four digits are used in the page field.[2]
+      
+      
+      A cellular automaton is a collection of "colored" 
+                      cells on a grid of specified shape that evolves through
+              a number of discrete time steps according to a set of rules based on the states of neighboring cells.
+           The rules are then applied iteratively for as many time steps as desired.
+
+Cellular Automaton -- from Wolfram MathWorldhttps://mathworld.wolfram.com › CellularAutomaton
